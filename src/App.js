8f005969a96aa364663e8d138e39f0f2f64e415a
Alejandro restrepo <alejandrorn.dev@gmail.com>
2024-10-09 00:03:22 -0500

    feat:se agrega nueva ruta para registrar los usuarios.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Layout from './components/layout';
 import Home from './pages/home';
 import Detail from './pages/detail';
+import RegisterUser from './pages/registrerUsers';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path='/registrarse' element={<RegisterUser />} />
           <Route path='/:slug' element={<Detail />} />
         </Route>
       </Routes>
